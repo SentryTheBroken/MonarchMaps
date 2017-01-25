9fc88c2ce9822719a96ff66b3c0487f67d8db09f
vjhsieh <vjhsieh@outlook.com>
2017-01-25 12:59:29 -0500

    Vivian Test Commit 1

---
+++ VivianTest1/VivianTest1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VivianTest1.Startup))]
+namespace VivianTest1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
